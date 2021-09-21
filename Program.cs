9b1c97351c9e5e48b590c413814b51b7616d3e94
SdavyACR <63772415+sibbyd@users.noreply.github.com>
2021-09-21 20:43:47 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace MySampleWebProject
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //same changes
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
